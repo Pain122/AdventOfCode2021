c54ef09164b32e0a8252aa884077b1e3ace5fbc5
Pain122 <42460516+Pain122@users.noreply.github.com>
2021-12-03 08:09:47 +0300

    Added solution to task 1
    Both scala and python

---
+++ Day1/untitled/build.sbt
@@ -0,0 +1,10 @@
+name := "untitled"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
+
+//scalacOptions += "-Ypartial-unification"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.6.1"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "3.3.0"
---
+++ Day1/untitled/src/main/scala/Solution.scala
@@ -0,0 +1,28 @@
+import Solution.readStr
+import cats.effect.{ExitCode, IO, IOApp}
+
+import scala.annotation.tailrec
+import scala.io.BufferedSource
+
+object Solution {
+  def readStr(path: String = "file.txt"): List[Int] = {
+    val source: BufferedSource = scala.io.Source.fromFile(path)
+    try source.mkString.split("\r\n").toList.map(_.toInt) finally source.close()
+  }
+}
+
+object MainApp extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    @tailrec
+    def calculateScore(list: List[Int], acc: Int): Int = {
+      list match {
+        case head :: next :: tail => if (head < next) calculateScore(next :: tail, acc + 1) else calculateScore(next :: tail, acc)
+        case _ => acc
+      }
+    }
+    val list = readStr("dataset.txt")
+    print(calculateScore(list, 0))
+    IO(ExitCode.Success)
+  }
+}
