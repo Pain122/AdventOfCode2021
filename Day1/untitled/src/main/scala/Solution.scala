033ae919850e338cf0e94a09aa54555132e4244a
Pain122 <42460516+Pain122@users.noreply.github.com>
2021-12-03 08:24:54 +0300

    Finished part 2
    Finished part 2 of day 1

--- Day1/untitled/src/main/scala/Solution.scala
+++ Day1/untitled/src/main/scala/Solution.scala
@@ -26,3 +26,19 @@ object MainApp extends IOApp {
     IO(ExitCode.Success)
   }
 }
+
+object Part2 extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    @tailrec
+    def calculateScore(list: List[Int], acc: Int): Int = {
+      list match {
+        case head :: next :: tail => if (head < next) calculateScore(next :: tail, acc + 1) else calculateScore(next :: tail, acc)
+        case _ => acc
+      }
+    }
+    val list = readStr("dataset.txt")
+    val newList = list.sliding(3, 1).map(_.sum).toList
+    print(calculateScore(newList, 0))
+    IO(ExitCode.Success)
+  }
+}
